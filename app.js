a64cd2599983f1d8c0ad4b3e8a68eb628912cef7
Ryle Edwards <ryle.edwards@gmail.com>
2022-04-20 22:32:53 -0500

    Set up refreshDisplay and creation of book ele

--- app.js
+++ app.js
@@ -1,5 +1,92 @@
+const buttons = document.querySelectorAll(".btn");
+const bookDisplay = document.querySelector(".book-display");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    btnHandler(button.classList[1]);
+  });
+});
+
+// Instantiate array to hold book objects
 let myLibrary = [];
 
-function Book() {}
+// Book constructor
+function Book(title, author, year, boolRead) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.boolRead = boolRead;
+}
 
-function addBookToLibrary() {}
+Book.prototype.toggleRead = function () {
+  this.boolRead = !this.boolRead;
+};
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+let theHobbit = new Book("The Hobbit", "J.R.R", 1958, false);
+let vampire = new Book("Interview with the Vampire", "Anne Rice", 1976, false);
+let it = new Book("It", "Stephen King", 1986, true);
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(vampire);
+addBookToLibrary(it);
+
+refreshDisplay();
+
+function btnHandler(btnClass) {
+  switch (btnClass) {
+    case "search":
+      console.log("search btn hit");
+      break;
+    case "remove":
+      console.log("remove btn hit");
+      break;
+    case "boolRead":
+      console.log("boolRead btn hit");
+      break;
+  }
+}
+
+function refreshDisplay() {
+  myLibrary.forEach((book) => {
+    createBookElement(book);
+  });
+}
+
+function createBookElement(book) {
+  /* Create document elements that comprise a book card */
+  const divBook = document.createElement("div");
+  const pTitle = document.createElement("p");
+  const btnRemove = document.createElement("button");
+  const iXMark = document.createElement("i");
+  const pAuthor = document.createElement("p");
+  const pYear = document.createElement("p");
+  const btnRead = document.createElement("button");
+  const iCheck = document.createElement("i");
+  /* Assign classes to elements */
+  divBook.classList += "book";
+  pTitle.classList += "title";
+  btnRemove.classList += "btn remove";
+  iXMark.classList += "fa-solid fa-xmark";
+  pAuthor.classList += "author";
+  pYear.classList += "year";
+  btnRead.classList += "btn boolRead";
+  iCheck.classList += "fa-solid fa-check";
+  /* Populate element text with book details */
+  pTitle.innerText = book.title;
+  pAuthor.innerText = book.author;
+  pYear.innerText = book.year;
+  //placeholder for btnRead logic.
+  divBook.appendChild(pTitle);
+  btnRemove.appendChild(iXMark);
+  divBook.appendChild(btnRemove);
+  divBook.appendChild(pAuthor);
+  divBook.appendChild(pYear);
+  btnRead.appendChild(iCheck);
+  divBook.appendChild(btnRead);
+  /* Final append of book into display */
+  bookDisplay.appendChild(divBook);
+}
